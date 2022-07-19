393d0aecc59727f2d75543c9fc77d93f7dbb1ab0
ucwealth <ucheokeke16@gmail.com>
2022-07-19 08:34:08 +0100

    create shared scheme

--- Decafit/DecafitTests/DecafitTests.swift
+++ Decafit/DecafitTests/DecafitTests.swift
@@ -21,13 +21,8 @@ class DecafitTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        let name = "dev"
+        XCTAssert(name=="dev", "Your name is wrong!")
     }
 
 }
