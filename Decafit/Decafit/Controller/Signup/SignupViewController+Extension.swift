0871de87fddb844b46fc3bf69486f2b29a0a6c7f
ucwealth <ucheokeke16@gmail.com>
2022-07-20 12:42:41 +0100

    create signup views and controls

--- Decafit/Decafit/Controller/Signup/SignupViewController+Extension.swift
+++ Decafit/Decafit/Controller/Signup/SignupViewController+Extension.swift
@@ -8,6 +8,58 @@
 import UIKit
 
 extension SignupViewController {
+    func setUpSubviews() {
+        let scrollView = UIScrollView()
+        view.addSubview(scrollView)
+        let parentStack = UIStackView(arrangedSubviews: [topImageView, signInStack, textViewStack, lineStack, socialStack, redirectToSignupStack])
+        parentStack.axis = .vertical
+        parentStack.alignment = .center
+        parentStack.distribution = .fill
+        parentStack.spacing = 20
+        parentStack.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.addSubview(parentStack)
+        
+        NSLayoutConstraint.activate([
+//            topImageView.heightAnchor.constraint(equalToConstant: 120),
+
+            
+            parentStack.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            parentStack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            parentStack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+//            parentStack.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+//            [fullNameTextField, phoneNumberTextField, emailTextField, passwordTextField, confirmPasswordTextField, signInButton, titleField].forEach {$0.heightAnchor.constraint(equalToConstant: 56)}
+            
+            fullNameTextField.heightAnchor.constraint(equalToConstant: 56),
+            phoneNumberTextField.heightAnchor.constraint(equalToConstant: 56),
+            emailTextField.heightAnchor.constraint(equalToConstant: 56),
+            passwordTextField.heightAnchor.constraint(equalToConstant: 56),
+            confirmPasswordTextField.heightAnchor.constraint(equalToConstant: 56),
+            signInButton.heightAnchor.constraint(equalToConstant: 64),
+            titleField.heightAnchor.constraint(equalToConstant: 45),
+            
+            fullNameTextField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85),
+            phoneNumberTextField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85),
+            emailTextField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85),
+            passwordTextField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85),
+            confirmPasswordTextField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85),
+            signInButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85),
+            titleField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.80),
+            
+            createPlanLabel.leadingAnchor.constraint(equalTo: topImageView.leadingAnchor, constant: 35),
+            createPlanLabel.trailingAnchor.constraint(equalTo: topImageView.trailingAnchor, constant: -40),
+            createPlanLabel.bottomAnchor.constraint(equalTo: topImageView.bottomAnchor, constant: -50),
+
+
+        ])
+    }
     
 }
 
+extension SignupViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        emailTextField.resignFirstResponder()
+        passwordTextField.resignFirstResponder()
+        return true
+    }
+}
--- Decafit/Decafit/Controller/Signup/SignupViewController.swift
+++ Decafit/Decafit/Controller/Signup/SignupViewController.swift
@@ -11,7 +11,283 @@ class SignupViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.backgroundColor = .white
+        setUpSubviews()
     }
-
+    
+    // MARK: - Image View
+    lazy var topImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "signup-img")
+        imageView.contentMode = .scaleAspectFill
+        imageView.tintColor = .white
+        imageView.addSubview(createPlanLabel)
+        return imageView
+    }()
+    
+    // MARK: - Title field
+    lazy var titleField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.backgroundColor = .clear
+        textField.text = "Sign in"
+        textField.font = customFont(size: 24, font: .poppinsMedium)
+        textField.textAlignment = .left
+        textField.tintColor = CustomColor.decafitBlack.color
+        textField.isEnabled = false
+        return textField
+    }()
+    
+    // MARK: - FullnameTextField
+    lazy var fullNameTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.autocapitalizationType = .none
+        textField.backgroundColor = .clear
+        textField.delegate = self
+        textField.placeholder = "Full name"
+        textField.font = customFont(size: 16, font: .poppinsRegular)
+        textField.layer.borderWidth = 1
+        textField.layer.cornerRadius = 5
+        textField.layer.borderColor = CustomColor.decafitGray.color.cgColor
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: textField.frame.height))
+        textField.leftViewMode = .always
+//        textField.addTarget(self, action: #selector(handleTextChange), for: .editingChanged)
+        return textField
+    }()
+    
+    // MARK: - PhoneNumberTextField
+    lazy var phoneNumberTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.autocapitalizationType = .none
+        textField.backgroundColor = .clear
+        textField.delegate = self
+        textField.placeholder = "Phone number"
+        textField.font = customFont(size: 16, font: .poppinsRegular)
+        textField.layer.borderWidth = 1
+        textField.layer.cornerRadius = 5
+        textField.layer.borderColor = CustomColor.decafitGray.color.cgColor
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: textField.frame.height))
+        textField.leftViewMode = .always
+//        textField.addTarget(self, action: #selector(handleTextChange), for: .editingChanged)
+        return textField
+    }()
+    
+    // MARK: - EmailTextField
+    lazy var emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.autocapitalizationType = .none
+        textField.backgroundColor = .clear
+        textField.delegate = self
+        textField.placeholder = "Email address"
+        textField.font = customFont(size: 16, font: .poppinsRegular)
+        textField.layer.borderWidth = 1
+        textField.layer.cornerRadius = 5
+        textField.layer.borderColor = CustomColor.decafitGray.color.cgColor
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: textField.frame.height))
+        textField.leftViewMode = .always
+//        textField.addTarget(self, action: #selector(handleTextChange), for: .editingChanged)
+        return textField
+    }()
+    
+    // MARK: - PasswordTextField
+    lazy var passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.autocapitalizationType = .none
+        textField.isSecureTextEntry = true
+        textField.backgroundColor = .clear
+        textField.delegate = self
+        textField.placeholder = "Password"
+        textField.font = customFont(size: 16, font: .poppinsRegular)
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = CustomColor.decafitGray.color.cgColor
+        textField.layer.cornerRadius = 5
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: textField.frame.height))
+        textField.leftViewMode = .always
+        textField.rightView = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: textField.frame.height))
+        textField.rightViewMode = .always
+//        textField.addTarget(self, action: #selector(handleTextChange), for: .editingChanged)
+        return textField
+    }()
+    
+    // MARK: - ConfirmPasswordTextField
+    lazy var confirmPasswordTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.autocapitalizationType = .none
+        textField.isSecureTextEntry = true
+        textField.backgroundColor = .clear
+        textField.delegate = self
+        textField.placeholder = "Confirm password"
+        textField.font = customFont(size: 16, font: .poppinsRegular)
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = CustomColor.decafitGray.color.cgColor
+        textField.layer.cornerRadius = 5
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: textField.frame.height))
+        textField.leftViewMode = .always
+        textField.rightView = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: textField.frame.height))
+        textField.rightViewMode = .always
+//        textField.addTarget(self, action: #selector(handleTextChange), for: .editingChanged)
+        return textField
+    }()
+    
+    // MARK: - SignupButton
+    lazy var signUpButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Sign up", for: .normal)
+        button.titleLabel?.font = customFont(size: 24, font: .ubuntuMedium)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.isEnabled = false
+        button.backgroundColor = CustomColor.decafitPurple.color
+        button.layer.cornerRadius = 5
+//        button.addTarget(self, action: #selector(handleLogin), for: .touchUpInside)
+        return button
+    }()
+    
+    // MARK: - Social Login Buttons
+    lazy var googleButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.borderWidth = 1
+        button.layer.borderColor = CustomColor.decafitGray.color.cgColor
+        button.contentEdgeInsets = UIEdgeInsets(top: 15, left: 15, bottom: 15, right: 15)
+        button.setImage(UIImage(named: "google-logo"), for: .normal)
+        button.layer.cornerRadius = 3
+//        button.addTarget(self, action: #selector(handleLogin), for: .touchUpInside)
+        return button
+    }()
+    
+    lazy var facebookButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(named: "fb-img"), for: .normal)
+        button.contentEdgeInsets = UIEdgeInsets(top: 15, left: 15, bottom: 15, right: 15)
+        button.layer.borderWidth = 1
+        button.layer.borderColor = CustomColor.decafitGray.color.cgColor
+        button.layer.cornerRadius = 3
+//        button.addTarget(self, action: #selector(handleLogin), for: .touchUpInside)
+        return button
+    }()
+    
+    lazy var appleButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.contentEdgeInsets = UIEdgeInsets(top: 15, left: 15, bottom: 15, right: 15)
+        button.setImage(UIImage(named: "apple-img"), for: .normal)
+        button.layer.borderWidth = 1
+        button.layer.borderColor = CustomColor.decafitGray.color.cgColor
+        button.layer.cornerRadius = 3
+//        button.addTarget(self, action: #selector(handleLogin), for: .touchUpInside)
+        return button
+    }()
+    
+    // MARK: - Sign In CTA Button Link
+    lazy var signInButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Sign in", for: .normal)
+        button.titleLabel?.font = customFont(size: 16, font: .poppinsRegular)
+        button.setTitleColor( CustomColor.decafitOrange.color , for: .normal)
+        button.backgroundColor = .clear
+//        button.addTarget(self, action: #selector(handleSignUp), for: .touchUpInside)
+        return button
+    }()
+    
+    // MARK: - LABELS
+    var createPlanLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Create a \nfitness/nutrition plan"
+        label.font = customFont(size: 32, font: .poppinsMedium)
+        label.textColor = .white
+        label.numberOfLines = 2
+        return label
+    }()
+    
+    var signInWithLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "  Or sign in with  "
+        label.font = customFont(size: 13, font: .poppinsMedium)
+        label.textColor = CustomColor.decafitGray.color
+        return label
+    }()
+    
+    var alreadyHaveAnAccountLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Already have an account? "
+        label.font = customFont(size: 16, font: .poppinsMedium)
+        label.textColor = CustomColor.decafitBlack.color
+        return label
+    }()
+    
+    // MARK: - Stack Views
+    lazy var signInStack: UIStackView = {
+       let stackview = UIStackView()
+        stackview.alignment = .leading
+        stackview.axis = .horizontal
+        stackview.addArrangedSubview(titleField)
+       return stackview
+    }()
+    
+    lazy var textViewStack: UIStackView = {
+       let stackview = UIStackView()
+        stackview.alignment = .leading
+        stackview.axis = .vertical
+        stackview.distribution = .equalSpacing
+        stackview.spacing = 20
+        stackview.addArrangedSubview(fullNameTextField)
+        stackview.addArrangedSubview(phoneNumberTextField)
+        stackview.addArrangedSubview(emailTextField)
+        stackview.addArrangedSubview(passwordTextField)
+        stackview.addArrangedSubview(confirmPasswordTextField)
+        stackview.addArrangedSubview(signUpButton)
+       return stackview
+    }()
+    
+    lazy var socialStack: UIStackView = {
+       let stackview = UIStackView()
+        stackview.alignment = .center
+        stackview.axis = .horizontal
+        stackview.distribution = .fillEqually
+        stackview.spacing = 35
+        stackview.addArrangedSubview(googleButton)
+        stackview.addArrangedSubview(facebookButton)
+        stackview.addArrangedSubview(appleButton)
+       return stackview
+    }()
+    
+    lazy var redirectToSignupStack: UIStackView = {
+       let stackview = UIStackView()
+        stackview.alignment = .center
+        stackview.axis = .horizontal
+        stackview.distribution = .fillProportionally
+        stackview.addArrangedSubview(alreadyHaveAnAccountLabel)
+        stackview.addArrangedSubview(signInButton)
+       return stackview
+    }()
+    
+    lazy var lineStack: UIStackView = {
+       let stackview = UIStackView()
+        stackview.alignment = .center
+        stackview.axis = .horizontal
+        stackview.distribution = .fillEqually
+        stackview.spacing = 20
+        let line = UIProgressView()
+        line.heightAnchor.constraint(equalToConstant: 1.2).isActive = true
+        let line2 = UIProgressView()
+        line2.heightAnchor.constraint(equalToConstant: 1.2).isActive = true
+        stackview.addArrangedSubview(line)
+        stackview.addArrangedSubview(signInWithLabel)
+        stackview.addArrangedSubview(line2)
+       return stackview
+    }()
+    
 }
+
