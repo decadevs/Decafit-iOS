e880f7fdc3410a62fb6fef2d13dfecdd30776a4b
ucwealth <ucheokeke16@gmail.com>
2022-08-05 14:04:21 +0100

    feat: create exercise timer file

---
+++ Decafit/Decafit/Controller/Exercises/ExerciseTimerViewController.swift
@@ -0,0 +1,10 @@
+
+import UIKit
+
+class ExerciseTimerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
