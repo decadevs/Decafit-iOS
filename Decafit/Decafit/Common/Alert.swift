4d351ac1a735f0e041753e81ea68036e99b0885c
ucwealth <ucheokeke16@gmail.com>
2022-08-11 11:02:24 +0100

    make signup calls

--- Decafit/Decafit/Common/Alert.swift
+++ Decafit/Decafit/Common/Alert.swift
@@ -6,9 +6,9 @@
 //
 import UIKit
 class Alert {
-    static func showAlert(_ inVC: UIViewController, title: String, message: String) {
+    static func showAlert(_ presenter: UIViewController, title: String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-        inVC.present(alert, animated: true, completion: nil)
+        presenter.present(alert, animated: true, completion: nil)
     }
 }
